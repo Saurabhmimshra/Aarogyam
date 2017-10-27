351c89c8edbba71aebd2a33bfbfcf5f83ffc90e6
Sanchit-Aggarwal <sancool1995@gmail.com>
2017-10-28 01:51:34 +0530

    appointment_finalise

--- appointment.php
+++ appointment.php
@@ -13,7 +13,7 @@
 
   <body>
     <div class="greener">
-    <img src="appointment.png" alt="appointment"><br><br>
+    <img src="appointment.png" height="300vw%" width="100%vw" alt="appointment"><br><br>
 
   <div class="flexer container">
     <div class="col-md-3"><label for="hospital"><h4><strong>Select Hospital</strong><h4></label><select id="hospital" class="form-control"> <option value="Male">Male</option> <option value="Female">Female</option> <option value="Others">Others</option> </select>
@@ -26,7 +26,13 @@
     </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
     <div class="col-md-1"></div>
 
-<h4>Enter your</h4>
+  </div>
+  <div class="container">
+    <label for="aar_id">Enter your Aarogyam ID</label><input id="first_name" class="form-control" maxlength="40" name="name" size="20" type="text" required />
+    <label for="date">Date of appointment</label><input id="first_name" class="form-control" maxlength="40" name="name" size="20" type="date" required />
+<br>
+    <button type="Book" name="button" class="form-control" onclick="">Confirm Appointment</button>
+
   </div>
   </div>
     </body>
