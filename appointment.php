185b990ffc45c4a855a8f62aff2849530b459de7
Sanchit-Aggarwal <sancool1995@gmail.com>
2017-10-28 01:02:44 +0530

    appointment update

---
+++ appointment.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">   <!-- salesforce linkage -->
+<link rel="stylesheet" href="appointment.css">
+   <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="druggist.css">
+      <title></title>
+</head>
+
+  <body>
+    <div class="greener">
+    <img src="appointment.png" alt="appointment"><br><br>
+
+  <div class="flexer container">
+    <div class="col-md-3"><label for="hospital"><h4><strong>Select Hospital</strong><h4></label><select id="hospital" class="form-control"> <option value="Male">Male</option> <option value="Female">Female</option> <option value="Others">Others</option> </select>
+    </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <div class="col-md-1"></div>
+    <div class="col-md-3"><label for="department"><h4><strong>Select Department</strong><h4></label><select id="hospital" class="form-control"> <option value="Male">Male</option> <option value="Female">Female</option> <option value="Others">Others</option> </select>
+    </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <div class="col-md-1"></div>
+    <div class="col-md-3"><label for="doctor"><h4><strong>Select Doctor</strong><h4></label><select id="hospital" class="form-control"> <option value="Male">Male</option> <option value="Female">Female</option> <option value="Others">Others</option> </select>
+    </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <div class="col-md-1"></div>
+
+<h4>Enter your</h4>
+  </div>
+  </div>
+    </body>
+</html>
