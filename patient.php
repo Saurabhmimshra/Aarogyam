e2e8054521795bcaf8467b5d7704f74b846d2d42
Sanchit-Aggarwal <sancool1995@gmail.com>
2017-10-27 21:42:15 +0530

    form_complete

--- patient.php
+++ patient.php
@@ -12,18 +12,19 @@
   </head>
   <body>
 <div class="container">
-<form>
+
   <center><label><h3>Appointment Type</h3></label><br></center>
   <div>
   <div class="col-sm-6">
     <button type="button" name="New_Appointment" class="form-control" onclick="new_user()">New Appointment</button>
       </div>
       <div class="col-sm-6">
-        <button type="button" name="Existing_Appointment" class="form-control">Existing Appointment</button>
+        <button type="button" name="Existing_Appointment" class="form-control" onclick="existing_user()">Existing Appointment</button>
           </div>
     </div>
     <br>
     <div id="form">
+
     </div>
 </div>
 
@@ -36,5 +37,13 @@
     </div>
 </footer>
   </body>
-  
+  <script type="text/javascript">
+  function new_user() {
+    document.getElementById("form").innerHTML='<form><label for="id">Aarogyam ID</label><input id="id" class="form-control" maxlength="40" name="id" size="20" type="text" required /><br><label for="first_name">Name</label><input id="first_name" class="form-control" maxlength="40" name="first_name" size="20" type="text" required /><br><label for="age">Age</label><input id="age" maxlength="40" class="form-control" name="age" size="20" type="number"/><br><label for="sex">Sex</label><select class="form-control" id="sex" name="sex"><option value="">--None--</option><option value="Male">Male</option><option value="Female">Female</option><option value="Others">Others</option></select><label for="diagnostic">Diagnostic Info</label><textarea class="form-control" name="diagnostic" required></textarea><br><label for="prescription">Prescription</label><textarea class="form-control" name="prescription" required></textarea><br><label for="duration">Duration</label><input id="duration" maxlength="20" name="duration" class="form-control" size="20" type="number" required/><br><label for="next_appointment">Next Appointment Date</label><input id="next_appointment" maxlength="40" class="form-control" name="next_appointment" size="20" type="text" required/><br><label for="special">Special Care</label><textarea class="form-control" name="special" required></textarea><br><input type="submit" class="form-control" name="submit" required></form>';
+  }
+  function existing_user() {
+    document.getElementById("form").innerHTML='<form><label for="id">Aarogyam ID</label><input id="id" class="form-control" maxlength="40" name="id" size="20" type="text" required /><br><button type="button" class="form-control" name="load_info">Fetch Information</button><br><label for="first_name">Name</label><input id="first_name" class="form-control" maxlength="40" name="first_name" size="20" type="text" required /><br><label for="age">Age</label><input id="age" maxlength="40" class="form-control" name="age" size="20" type="number"/><br><label for="sex">Sex</label><select class="form-control" id="sex" name="sex"><option value="">--None--</option><option value="Male">Male</option><option value="Female">Female</option><option value="Others">Others</option></select><label for="diagnostic">Diagnostic Info</label><textarea class="form-control" name="diagnostic" required></textarea><br><label for="prescription">Prescription</label><textarea class="form-control" name="prescription" required></textarea><br><label for="duration">Duration</label><input id="duration" maxlength="20" name="duration" class="form-control" size="20" type="number" required/><br><label for="next_appointment">Next Appointment Date</label><input id="next_appointment" maxlength="40" class="form-control" name="next_appointment" size="20" type="text" required/><br><label for="special">Special Care</label><textarea class="form-control" name="special" required></textarea><br><input type="submit" class="form-control" name="submit" required></form>';
+  }
+
+  </script>
 </html>
