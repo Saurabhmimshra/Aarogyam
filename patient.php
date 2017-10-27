7108effe361e83c5a2dcde5dfdaf06abc8419d09
Sanchit-Aggarwal <sancool1995@gmail.com>
2017-10-27 20:47:38 +0530

    form

---
+++ patient.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">   <!-- salesforce linkage -->
+
+     <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
+      <link rel="stylesheet" href="home.css">
+      <link rel="stylesheet" href="contact.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <title></title>
+  </head>
+  <body>
+
+<form>
+
+
+<input type="submit"  class="form-control" name="submit" required>
+
+</form>
+
+</form>
+<footer style="padding: 20px; border-top: 1px solid #eee; margin-top: 25px;">
+  <div class="container">
+  <div class="text-muted text-center">
+    Copyright © Sanchit Aggarwal<br><center>#9560766238</center>
+  </div>
+    </div>
+</footer>
+  </body>
+</html>
