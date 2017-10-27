026f3d4f62243292d84246370ccdf9917ba6a0ad
Sanchit-Aggarwal <sancool1995@gmail.com>
2017-10-27 23:43:32 +0530

    druggist_update

--- druggist.php
+++ druggist.php
@@ -8,21 +8,42 @@
     <link rel="stylesheet" href="contact.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
       <meta charset="utf-8">
+      <link rel="stylesheet" href="druggist.css">
       <title></title>
 </head>
+
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">WebSiteName</a>
-        </div>
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Page 1</a></li>
-          <li><a href="#">Page 2</a></li>
-          <li><a href="#">Page 3</a></li>
-        </ul>
-      </div>
-    </nav>
+    <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">MY ORDERS</a>
+  </div>
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+    <button type="button"  class="form-control" name="button" onclick="">bjhbhj</button><br>
+
+
+</nav>
     </body>
 </html>
